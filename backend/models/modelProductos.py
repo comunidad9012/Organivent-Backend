6421e44a75e15c7849b21e7d77ab96478c2814bd
sofisando <sofisandobal9@gmail.com>
2024-08-22 02:55:06 -0300

    funcional con nuestra base de datos

--- backend/models/modelProductos.py
+++ backend/models/modelProductos.py
@@ -4,41 +4,41 @@ from bson import json_util
 from bson.objectid import ObjectId
 import re
     
-class NewsModel:
+class ProductosModel:
     def __init__(self, app):
         self.mongo = PyMongo(app)
 
-    def create_news(self, data):
+    def create_Productos(self, data):
         if 'noticia' in data:
-            news_data = {'noticia': data['noticia'],'titulo':data['titulo'], 'fecha':data['fecha'],'miniatura':data['miniatura']}
-            self.mongo.db.news.insert_one(news_data)
+            Productos_data = {'noticia': data['noticia'],'titulo':data['titulo'], 'fecha':data['fecha'],'miniatura':data['miniatura']}
+            self.mongo.db.Productos.insert_one(Productos_data)
             return {"contenido": "exitoso"}
         else:
             return {"contenido": "no funciona"}
         
-    def show_news(self):
-        news=list(self.mongo.db.news.find().sort('_id', -1))
-        for item in news:
+    def show_Productos(self):
+        Productos=list(self.mongo.db.Productos.find().sort('_id', -1))
+        for item in Productos:
             item['_id'] = str(item['_id'])
-        response=json_util.dumps(news)
+        response=json_util.dumps(Productos)
         return Response(response, mimetype="application/json")
 
-    def specific_new(self,id):
-        news=self.mongo.db.news.find_one({'_id': ObjectId(id), })
-        response=json_util.dumps(news)
+    def specific_product(self,id):
+        Productos=self.mongo.db.Productos.find_one({'_id': ObjectId(id), })
+        response=json_util.dumps(Productos)
         return Response(response, mimetype="application/json")
     
-    def find_news(self, palabra):
+    def find_Productos(self, palabra):
         regex = re.compile(f".*{re.escape(palabra)}.*", re.IGNORECASE)
-        news = list(self.mongo.db.news.find({
+        Productos = list(self.mongo.db.Productos.find({
             "$or": [
                 {"titulo": regex},
                 {"noticia": regex}
             ]
         }).sort('_id', -1))
         
-        for item in news:
+        for item in Productos:
             item['_id'] = str(item['_id'])
         
-        response = json_util.dumps(news)
+        response = json_util.dumps(Productos)
         return Response(response, mimetype="application/json")
